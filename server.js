d0c10de15c09b2e8c511162f8c2ee0a3a9afb08c
vivekkhatik617-dotcom <vivekkhatik617@gmail.com>
2026-06-23 14:18:13 +0530

    fixed supabase key

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const { createClient } = require('@supabase/supabase-js');
 const app = express();
 const supabase = createClient(
     process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
+    process.env.SUPABASE_KEY
 );
 const upload = multer({ storage: multer.memoryStorage() });
 
